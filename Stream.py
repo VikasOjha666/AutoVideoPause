7c33c556a5e9b35f24b25632681e7301a2cd36c8
VikasOjha666 <vikas.ojha894@gmail.com>
2020-04-11 20:07:59 +0530

    Made satisfying code

--- Stream.py
+++ Stream.py
@@ -1,56 +1,37 @@
 import cv2
-import time
 import numpy as np
-from ffpyplayer.player import MediaPlayer
-import sys
-
-
-videoreader=cv2.VideoCapture(sys.argv[1])
-player=MediaPlayer(sys.argv[1])
+import os
+import subprocess
+import pyautogui
 
 cap=cv2.VideoCapture(0)
+is_paused=0
+
 face_cascade = cv2.CascadeClassifier('frontalfacehaarcascade.xml')
 
-eye_cascade = cv2.CascadeClassifier('eyeshaarcascade.xml')
+found_face=False
 
-pauseframe=cv2.imread('PauseFrame.png',0)
-pauseframe=cv2.resize(pauseframe,(640,360))
-
-
-global found_eyes
-found_eyes=False
-
-while videoreader.isOpened():
-    ret,frame=videoreader.read()
-    audio_frame,val=player.get_frame()
-    ret, img = cap.read()
+while True:
+    ret,img=cap.read()
+    if ret is not True:
+        print("Frame processing error")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    #print(len(faces))
     if len(faces)==0:
-        found_eyes=False
+        found_face=False
     else:
-        found_eyes=True
+        found_face=True
 
-
-    if not ret:
-        print("Can't read file")
-
-    #Pausing stuff
-    if found_eyes==False:
-        frame=pauseframe
+    if is_paused==0 and found_face==False:
+        pyautogui.typewrite(['space'],0.1)
+        is_paused=1
+    elif is_paused==1 and found_face==True:
+        pyautogui.typewrite(['space'],0.1)
+        is_paused=0
     else:
-        frame=frame
-
-    cv2.imshow('Playing Video',frame)
-    if val != 'eof' and audio_frame is not None:
-            #audio
-            img, t = audio_frame
+        pass
 
 
 
-    if cv2.waitKey(1)==ord('q'):
-        break
-videoreader.release()
+
 cap.release()
-cv2.destroyAllWindows()
